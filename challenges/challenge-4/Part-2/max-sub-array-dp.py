24c632cab16cc6d648600b5a51912aca19fb6838
daisukiyo <tk.rain09@gmail.com>
2019-08-05 16:00:03 -0700

    some cool fixes and documentation

--- challenges/challenge-4/Part-2/max-sub-array-dp.py
+++ challenges/challenge-4/Part-2/max-sub-array-dp.py
@@ -1,5 +1,17 @@
 def max_sub_array_sum(a, size):
+    """
+    Find the largest sum from the largest contiguous subarray
 
+    Parameters:
+    a (array): The array to find the largest sum from
+    size (int): The size of the given array
+
+    Returns:
+    max_so_far (int): The largest sum from a contiguous array
+
+    Runtime: 
+    O(n)
+    """
     # base case
     max_so_far = a[0]
     current_max = a[0]
@@ -12,11 +24,16 @@ def max_sub_array_sum(a, size):
         max_so_far = max(max_so_far, current_max)
     return max_so_far
 
+# some driver code
 if __name__ == "__main__": 
     a = [-2, 1, -3, 4, -1, 2, 1, -5, 4] 
+
+    # dp generated solution
     print('Solution:')
     print('For this input: ', a)
     print('The optimal solution is: ', max_sub_array_sum(a,len(a)) )
+
+    # hard coded reasoning
     print('\nReasoning:')
     print('Contiguous Subarray: {4, -1, 2, 1}') 
     
